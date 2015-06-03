36e9c7fe3d3e52a6cde007cb419611f408c01323
sebastien <sebastien.dabdoub@gmail.com>
2015-06-03 17:54:47 -0400

    Implements appendEntries rpc call and adds gitignore file
    There are some leftover prints in the rpc call, but they are
    tagged for removal. Ill remove them once I no longer need to debug
    the application or I can change them to real log statements

--- RaftNode.py
+++ RaftNode.py
@@ -37,18 +37,18 @@ class RaftNode:
         # other state
         self.role = Role.FOLLOWER
         self.port = port
-        
+        # a global lock for thread synhronization between the poll method
+        # and the rpc methods
+        self.lock = threading.Lock()
+
         # init server
         processor = Raft.Processor(self)
         transport = TSocket.TServerSocket(port=port)
-        #transport.setTimeout(1000) # 1s timeout
 
-        #self.server = TServer.TSimpleServer(processor, transport)
-        #self.server = TServer.TThreadPoolServer(processor, transport)
         self.server = ThreadPoolThriftServer(processor, transport)
 
         self.initialMembers = initialMembers
-        
+
     def start(self):
         # start listening thread
         self.listen_thread = threading.Thread(target=self._listen)
@@ -66,15 +66,15 @@ class RaftNode:
     def connect(self):
         # init other sockets to connect to other members
         if self.initialMembers != None:
-            self.initialMembers = initialMembers
             self.members = []
-            for (addr,port) in initialMembers:
+            for (addr,port) in self.initialMembers:
                 client = connectClient(addr,port)
 
                 self.members.append(client)
 
     def poll(self):
-        time.sleep(1)
+        with self.lock:
+            time.sleep(1)
 
     def stop(self):
         try:
@@ -85,16 +85,72 @@ class RaftNode:
 
     # The rpc methods
     def ping(self):
-        return 'pong'
+        # doesnt actually need the lock, but for good measure
+        with self.lock:
+            return 'pong'
 
     def appendEntries(self, term, leaderId, prevLogIndex, prevLogTerm, entries, leaderCommit):
-        pass
+        # TODO: remove
+        print 'called appendEntries'
+        # TODO: figure out what to do with leaderId
+        with self.lock:
+            ret = AppendReturn(self.currentTerm,False)
+            try:
+                # Reply false if term < currentTerm
+                if term < self.currentTerm:
+                    # TODO: remove
+                    print 'term is too old'
+                    return ret
+
+                # Reply false if log doesn't contain an entry at prevLogIndex
+                # whose term matches prevLogTerm
+                # case for empty log
+                if ( len(self.log) > 0 or prevLogIndex >= 0 ) and \
+                  (len(self.log) < prevLogIndex+1 or self.log[prevLogIndex][0] != prevLogTerm):
+                    # TODO: remove
+                    print 'no entry at prevLogIndex'
+                    return ret
+
+                # If an existing entry conflicts with a new one (same index
+                # but different terms), delete the existing entry and all that
+                # follow it
+                for i in range(prevLogIndex+1,len(self.log)):
+                    entryIndex = i - prevLogIndex - 1
+                    if entryIndex > len(entries):
+                        break
+                    # TODO: should only the term match?
+                    if self.log[i] != (term,entries[entryIndex]):
+                        self.log = self.log[:i]
+                        break
+
+                # Append any new entries not already in the log
+                insertIndex = prevLogIndex+1
+                for e in entries:
+                    if len(self.log) > insertIndex:
+                        self.log[insertIndex] = (term,e)
+                    else:
+                        self.log.append((term,e))
+
+                # If leaderCommit > commitIndex, set commitIndex =
+                # min(leaderCommit, index of last new entry)
+                if leaderCommit > self.commitIndex:
+                    self.commitIndex = min(leaderCommit,len(self.log) - 1)
+
+                # TODO: remove
+                print 'log is now',self.log
+                ret.success = True
+
+            except Exception as err:
+                print err
+            return ret
 
     def requestVote(self, term, candidateId, lastLogIndex, lastLogTerm):
-        pass
+        with self.lock:
+            pass
 
     def installSnapshot(self, term, leaderId, lastIncludedIndex, lastIncludedTerm, offset, data, done):
-        pass
+        with self.lock:
+            pass
 
 def connectClient(addr,port):
     # Make socket
@@ -108,12 +164,12 @@ def connectClient(addr,port):
 
     # Create a client to use the protocol encoder
     client = Raft.Client(protocol)
-    
+
     # Connect
     transport.open()
     return client
 
-        
+
 def runNode(port,members=None):
     node = RaftNode(port,members)
     node.start()
@@ -123,17 +179,19 @@ def runNode(port,members=None):
 
 if __name__ == '__main__':
     node = runNode(8080)
+    nodeB = runNode(8081,[('localhost',8080)])
 
     # make client
     client = connectClient('localhost',8080)
     print client.ping()
-    
+
     try:
         while True:
             node.poll()
+            nodeB.poll()
     except:
         pass
     finally:
         print 'stopping'
+        nodeB.stop()
         node.stop()
-        
